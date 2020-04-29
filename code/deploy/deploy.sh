144c6a1ba461d62dfacdfa3a527fa23c58a1f6a1
Eugene Fedorenko <eugene.fedor@gmail.com>
2020-04-29 10:11:06 -0700

    deploy

--- code/deploy/deploy.sh
+++ code/deploy/deploy.sh
@@ -1,25 +1,23 @@
-# az ml model deploy -n tacosandburritos -m tacosandburritos:1 --ic inferenceconfig.json --dc deploymentconfig.json --resource-group taco-rg --workspace-name taco-workspace --overwrite -v
 #!/bin/sh
-while getopts "m:s:p:u:r:w:t:x:" option;
+while getopts "m:u:r:w:x:d:" option;
     do
     case "$option" in
         m ) MODEL=${OPTARG};;
-        s ) SERVICE_PRINCIPAL_ID=${OPTARG};;
-        p ) SERVICE_PRINCIPAL_PASSWORD=${OPTARG};;
         u ) SUBSCRIPTION_ID=${OPTARG};;
         r ) RESOURCE_GROUP=${OPTARG};;
         w ) WORKSPACE=${OPTARG};;
-        t ) TENANT_ID=${OPTARG};;
         x ) RUN_ID=${OPTARG};;
+        d ) DEPLOYMENT_NAME=${OPTARG};;
     esac
 done
-# az login --service-principal --username ${SERVICE_PRINCIPAL_ID} --password ${SERVICE_PRINCIPAL_PASSWORD} -t $TENANT_ID
 echo $MODEL
 echo $WORKSPACE
 echo $RESOURCE_GROUP
 echo $SUBSCRIPTION_ID
-# model_id=$(az ml model list --model-name $MODEL --workspace-name $WORKSPACE -g $RESOURCE_GROUP --subscription-id $SUBSCRIPTION_ID --tag run_id=$RUN_ID | jq -r '.[0] | .id') 
+echo $RUN_ID
+echo $DEPLOYMENT_NAME
+az extension add -n azure-cli-ml
+model_id=$(az ml model list --model-name $MODEL --workspace-name $WORKSPACE -g $RESOURCE_GROUP --subscription-id $SUBSCRIPTION_ID --tag run_id=$RUN_ID | jq -r '.[0] | .id') 
 echo $model_id
+az ml model deploy -n $DEPLOYMENT_NAME -m $model_id --ic '/scripts/inferenceconfig.json'  --dc '/scripts/deploymentconfig.json' -w $WORKSPACE -g $RESOURCE_GROUP --overwrite -v
 
-# az ml model deploy -n "mexicanfood" -m $model_id --ic '/scripts/inferenceconfig.json'  --dc '/scripts/deploymentconfig.json' -w $WORKSPACE -g $RESOURCE_GROUP --overwrite -v
-az ml model deploy -h
