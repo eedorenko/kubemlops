de2d5de8f7072fe24525957c2d1a89331e025eb7
Eugene Fedorenko <eugene.fedor@gmail.com>
2020-04-28 18:48:52 -0700

    action

--- code/deploy/deploy.sh
+++ code/deploy/deploy.sh
@@ -1,19 +1,15 @@
 # az ml model deploy -n tacosandburritos -m tacosandburritos:1 --ic inferenceconfig.json --dc deploymentconfig.json --resource-group taco-rg --workspace-name taco-workspace --overwrite -v
 #!/bin/sh
-while getopts "m:n:i:d:s:p:u:r:w:t:b:x:" option;
+while getopts "m:s:p:u:r:w:t:x:" option;
     do
     case "$option" in
         m ) MODEL=${OPTARG};;
-        n ) MODEL_NAME=${OPTARG};;
-        i ) INFERENCE_CONFIG=${OPTARG};;
-        d ) DEPLOYMENTCONFIG=${OPTARG};;
         s ) SERVICE_PRINCIPAL_ID=${OPTARG};;
         p ) SERVICE_PRINCIPAL_PASSWORD=${OPTARG};;
         u ) SUBSCRIPTION_ID=${OPTARG};;
         r ) RESOURCE_GROUP=${OPTARG};;
         w ) WORKSPACE=${OPTARG};;
         t ) TENANT_ID=${OPTARG};;
-        b ) BASE_PATH=${OPTARG};;
         x ) RUN_ID=${OPTARG};;
     esac
 done
@@ -27,4 +23,4 @@ echo $SUBSCRIPTION_ID
 model_id=$(az ml model list --model-name $MODEL --workspace-name $WORKSPACE -g $RESOURCE_GROUP --subscription-id $SUBSCRIPTION_ID --tag run_id=$RUN_ID | jq -r '.[0] | .id') 
 echo $model_id
 
-az ml model deploy -n "mexicanfood" -m $model_id --ic $INFERENCE_CONFIG  --dc $DEPLOYMENTCONFIG -w $WORKSPACE -g $RESOURCE_GROUP --overwrite -v
+az ml model deploy -n "mexicanfood" -m $model_id --ic '/scripts/inferenceconfig.json'  --dc '/scripts/deploymentconfig.json' -w $WORKSPACE -g $RESOURCE_GROUP --overwrite -v
