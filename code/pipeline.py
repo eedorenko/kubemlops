0d45df019a8a541fbf0c9da71e8db62261c0c57a
Eugene Fedorenko <eugene.fedor@gmail.com>
2020-04-29 13:07:11 -0700

    ci

--- code/pipeline.py
+++ code/pipeline.py
@@ -114,10 +114,40 @@ def tacosandburritos_train(
     #         '--run_id', dsl.RUN_ID_PLACEHOLDER
     #     ]
     # ).apply(use_azure_secret())
-
     # operations['register'].after(operations['training'])
 
+    # # register model
+    # operations['register'] = dsl.ContainerOp(
+    #     name='register',
+    #     image=image_repo_name + '/register:latest',
+    #     command=['python'],
+    #     arguments=[
+    #         '/scripts/register.py',
+    #         '--base_path', persistent_volume_path,
+    #         '--model', 'latest.h5',
+    #         '--model_name', model_name,
+    #         '--tenant_id', "$(AZ_TENANT_ID)",
+    #         '--service_principal_id', "$(AZ_CLIENT_ID)",
+    #         '--service_principal_password', "$(AZ_CLIENT_SECRET)",
+    #         '--subscription_id', "$(AZ_SUBSCRIPTION_ID)",
+    #         '--resource_group', resource_group,
+    #         '--workspace', workspace,
+    #         '--run_id', dsl.RUN_ID_PLACEHOLDER
+    #     ]
+    # ).apply(use_azure_secret())
+    # operations['register'].after(operations['training'])
     
+        operations['finalize'] = dsl.ContainerOp(
+            name='Finalize',
+            image="curlimages/curl",
+            command=['curl'],
+            arguments=[
+                '-d', get_bot_payload("Model is registered"),
+                kubemlopsbot_svc 
+            ]
+        )
+        operations['finalize'].after(operations['preprocess'])
+
 
     # operations['deploy'] = dsl.ContainerOp(
     #     name='deploy',
