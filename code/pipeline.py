7bbe3692b37fc0b7805782dfb70991ecc58ff1c3
Eugene Fedorenko <eugene.fedor@gmail.com>
2020-04-29 18:39:21 -0700

    ci/cd

--- code/pipeline.py
+++ code/pipeline.py
@@ -95,37 +95,37 @@ def tacosandburritos_train(
         )
         operations['training'].after(operations['preprocess'])
 
-        # # register model
-        # operations['register'] = dsl.ContainerOp(
-        #     name='register',
-        #     image=image_repo_name + '/register:latest',
-        #     command=['python'],
-        #     arguments=[
-        #         '/scripts/register.py',
-        #         '--base_path', persistent_volume_path,
-        #         '--model', 'latest.h5',
-        #         '--model_name', model_name,
-        #         '--tenant_id', "$(AZ_TENANT_ID)",
-        #         '--service_principal_id', "$(AZ_CLIENT_ID)",
-        #         '--service_principal_password', "$(AZ_CLIENT_SECRET)",
-        #         '--subscription_id', "$(AZ_SUBSCRIPTION_ID)",
-        #         '--resource_group', resource_group,
-        #         '--workspace', workspace,
-        #         '--run_id', dsl.RUN_ID_PLACEHOLDER
-        #     ]
-        # ).apply(use_azure_secret())
-        # operations['register'].after(operations['training'])
+        # register model
+        operations['register'] = dsl.ContainerOp(
+            name='register',
+            image=image_repo_name + '/register:latest',
+            command=['python'],
+            arguments=[
+                '/scripts/register.py',
+                '--base_path', persistent_volume_path,
+                '--model', 'latest.h5',
+                '--model_name', model_name,
+                '--tenant_id', "$(AZ_TENANT_ID)",
+                '--service_principal_id', "$(AZ_CLIENT_ID)",
+                '--service_principal_password', "$(AZ_CLIENT_SECRET)",
+                '--subscription_id', "$(AZ_SUBSCRIPTION_ID)",
+                '--resource_group', resource_group,
+                '--workspace', workspace,
+                '--run_id', dsl.RUN_ID_PLACEHOLDER
+            ]
+        ).apply(use_azure_secret())
+        operations['register'].after(operations['training'])
     
-        # operations['finalize'] = dsl.ContainerOp(
-        #     name='Finalize',
-        #     image="curlimages/curl",
-        #     command=['curl'],
-        #     arguments=[
-        #         '-d', get_callback_payload("Model is registered"),
-        #         callback_url
-        #     ]
-        # )
-        # operations['finalize'].after(operations['register'])
+        operations['finalize'] = dsl.ContainerOp(
+            name='Finalize',
+            image="curlimages/curl",
+            command=['curl'],
+            arguments=[
+                '-d', get_callback_payload("Model is registered"),
+                callback_url
+            ]
+        )
+        operations['finalize'].after(operations['register'])
 
 
     # operations['deploy'] = dsl.ContainerOp(
