17066a17a2f469ea3701c50c612e91b608a7cbdc
Eugene Fedorenko <eugene.fedor@gmail.com>
2020-04-30 19:07:41 -0700

    Update azure-credentials.sh

--- setup/kfp/azure-credentials.sh
+++ setup/kfp/azure-credentials.sh
@@ -5,6 +5,6 @@ export AZ_CLIENT_SECRET=''
 export NAMESPACE='kubeflow'
 kubectl create secret generic azcreds --from-literal=AZ_SUBSCRIPTION_ID=$AZ_SUBSCRIPTION_ID \
                                       --from-literal=AZ_TENANT_ID=$AZ_TENANT_ID \
-                                      --from-literal=AZ_CLIENT_ID=$AZ_CLIENT_ID= \
+                                      --from-literal=AZ_CLIENT_ID=$AZ_CLIENT_ID \
                                       --from-literal=AZ_CLIENT_SECRET=$AZ_CLIENT_SECRET \
                                       -n $NAMESPACE
